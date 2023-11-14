61671b2a577325cf45c281c980509f7ec2ddb7f4
irehankhan <irehankhan@yahoo.com>
2023-11-14 17:28:37 +0500

    Modification

--- app.py
+++ app.py
@@ -3,11 +3,11 @@ import os
 
 app = Flask(__name__)
 
-app_name = os.environ.get('APP_NAME', 'EveryOne')
+app_name = os.environ.get('APP_NAME', 'World!')
 
 @app.route('/')
 def home():
-    return f'Hello, {app_name}!'
+    return f'Hello, {app_name}'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
